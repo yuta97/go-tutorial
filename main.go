df29e0d234ad1a9570b50f5041ce3b3be7814879
増井 悠太 <yuta970702m@gmail.com>
2020-12-20 14:39:27 +0900

    first commit
    go tutorial start

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import fmt "fmt" 
+
+func main() {
+		fmt.Printf("Hello, world;or こんにちは 世界\n")
+
+		var name string = "Bob"
+		fmt.Println(name)
+
+		var num int = 1
+		fmt.Println(num+num)
+}
