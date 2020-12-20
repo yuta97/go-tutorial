050fba128a5ebffdb03b2965f8ceb8a109d98af6
増井 悠太 <yuta970702m@gmail.com>
2020-12-20 14:48:27 +0900

    try new grammar if end etc

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import fmt "fmt" 
 
+const (
+	One   = 1
+	Two   = 2
+	Three = 3
+)
+
 func main() {
 		fmt.Printf("Hello, world;or こんにちは 世界\n")
 
@@ -10,4 +16,16 @@ func main() {
 
 		var num int = 1
 		fmt.Println(num+num)
+
+		var array1 [4]int
+		fmt.Println(array1)
+
+		fmt.Println(One)
+
+		a := 3
+		if a ==3 {
+			fmt.Println("a = 3")
+		} else {
+			fmt.Println("a ins not 3")
+		}
 }
